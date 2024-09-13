561973334f8d4672c6ccb0590f32b49fc3631c6d
UrjiiH <urjiihd@gmail.com>
2024-09-13 17:03:39 +0300

    Added background and localization

---
+++ .dart_tool/flutter_build/b656e287fe560f47d02dba95973c1b86/native_assets.d
@@ -0,0 +1,1 @@
+ C:\\Users\\urjii\\OneDrive\\Desktop\\pro\\flutter_application_1\\.dart_tool\\flutter_build\\b656e287fe560f47d02dba95973c1b86\\native_assets.yaml: 
---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,95 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get ticket => 'Ticket';
+
+  @override
+  String get aboutus => 'About us';
+
+  @override
+  String get profile => 'Profil';
+
+  @override
+  String get username => 'Username';
+
+  @override
+  String get announcement => 'Announcement';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get welcomeback => 'Welcome Back!';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get forgotpassword => 'Forgot password';
+
+  @override
+  String get login => 'Log in';
+
+  @override
+  String get contiunewithgoogle => 'Continue with Google';
+
+  @override
+  String get newusersignup => 'New users? Sign up';
+
+  @override
+  String get haveanaccount => 'Have an account? Log in';
+
+  @override
+  String get sofa => 'Sofa';
+
+  @override
+  String get diningtable => 'Dining Table';
+
+  @override
+  String get kitchencabinet => 'Kitchen Cabinet';
+
+  @override
+  String get tvstand => 'TV Stand';
+
+  @override
+  String get playnow => 'Play now';
+
+  @override
+  String get birr => 'Birr';
+
+  @override
+  String get upcomingtickets => 'Upcoming tickets';
+
+  @override
+  String get tickets => 'Tickets';
+
+  @override
+  String get createyouraccount => 'Create your account!';
+
+  @override
+  String get confirmpassword => 'Confirm password';
+
+  @override
+  String get date => 'Date';
+
+  @override
+  String get july => 'July';
+
+  @override
+  String get aboutustext => 'At [ App Name], we bring excitement and joy to every draw. Our mission is to offer you not just a chance to win, but an opportunity to transform your home with stunning furniture. Join us and take a step towards making your dream home a reality.';
+}
